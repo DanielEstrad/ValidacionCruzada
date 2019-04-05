810ea1841133fa91dd2ef37cc2ed2affb96df96f
DanielEstrad <46507075+DanielEstrad@users.noreply.github.com>
2019-04-04 23:33:13 -0600

    Ey

--- retroIntento.py
+++ retroIntento.py
@@ -45,19 +45,20 @@ class RedNeuronal:
     def entrenar(self, x, y, jP, kP,tasa_aprendizaje=0.2, epocas=100000):
         #Se agrega la columna de bias a las entradas X
         ones = np.atleast_2d(np.ones(x.shape[0]))
+        onesjP = np.atleast_2d(np.ones(jP.shape[0]))
         totalErrores = 0
         x = np.concatenate((ones.T, x), axis=1)
+        jP = np.concatenate((onesjP.T, jP), axis=1)
         errorTotal = 1
-        while (errorTotal > 0.00001):
+        while (errorTotal > 0.0001):
             for w in range(epocas):
                 #Se obtiene de forma aleatoria uno de los registros de entrada
                 i = np.random.randint(x.shape[0])
                 a = [x[i]]
-
                 for z in range(len(self.pesos)):
-                        valor = np.dot(a[z], self.pesos[z])
-                        activacion = self.activacion(valor)
-                        a.append(activacion)
+                    valor = np.dot(a[z], self.pesos[z])
+                    activacion = self.activacion(valor)
+                    a.append(activacion)
                 # Calculo de la diferencia en la capa de salida y el valor obtenido
                 error = y[i] - a[-1]
                 sumError = 0.5 * (error**2)
@@ -78,21 +79,19 @@ class RedNeuronal:
                     self.pesos[i] += tasa_aprendizaje * capa.T.dot(delta)
             print("Error MSEe: ",sumError)
             for wg in range(epocas):
-                i = np.random.randint(jP.shape[0])
-                aP = [jP[i]]
-
+                i2 = np.random.randint(jP.shape[0])
+                a2 = [jP[i2]]
                 for z in range(len(self.pesos)):
-                        valorP = np.dot(aP[z], self.pesos[z])
-                        activacionP = self.activacion(valorP)
-                        aP.append(activacion)
+                    valorP = np.dot(a2[z], self.pesos[z])
+                    activacionP = self.activacion(valorP)
+                    a2.append(activacionP)
                 # Calculo de la diferencia en la capa de salida y el valor obtenido
-                errorP = kP[i] - aP[-1]
-                secSumError = 0.5 * (error**2)
-                #print("Error otro Forward: ",sumError)
-                deltas = [errorP * self.activacion_prime(aP[-1])]
+                errorP = kP[i] - a2[-1]
+                secSumError = 0.5 * (errorP**2)
+                deltas = [errorP * self.activacion_prime(a2[-1])]
                 # Se empienza en la segunda capa
                 for k in range(len(a) - 2, 0, -1): 
-                    deltas.append(deltas[-1].dot(self.pesos[k].T)*self.activacion_prime(aP[k]))
+                    deltas.append(deltas[-1].dot(self.pesos[k].T)*self.activacion_prime(a2[k]))
                 #self.deltas.append(deltas)
             print("Error MSEp: ",secSumError)
             errorTotal = ((34*sumError)+(17*secSumError))/51
@@ -132,7 +131,7 @@ for a in range(len(conjuntoPruebaK1)):
 
 
 RN = RedNeuronal([1,3,1],activacion ='tanh')
-RN.entrenar(conjuntoK1, yK1, conjuntoPruebaK1, yPK1,tasa_aprendizaje=0.03,epocas=15000)
+RN.entrenar(conjuntoK1, yK1, conjuntoPruebaK1, yPK1,tasa_aprendizaje=0.03,epocas=10000)
  
 cont=0
 for resul in x:
